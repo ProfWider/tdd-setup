c1ab587c64918b37498900102d03394156772c97
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2020-10-08 22:35:29 +0200

    giving things names is always a good idea

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -1,10 +1,13 @@
 package tdd.setup;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Retro calculator")
 class CalculatorTest {
     @Test
+    @DisplayName("can do 2 + 2")
     void calculatorCanDoTwoPlusTwo() {
         Calculator calc = new Calculator();
         calc.pressDigitKey(2);

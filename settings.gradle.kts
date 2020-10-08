6b01aafa6f430d3b1c08f63956b7b66b3a4559e1
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2020-10-08 16:32:24 +0200

    saying yes to T&C in a very dev-vy way

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,14 @@
  */
 
 rootProject.name = "tdd-setup"
+
+plugins {
+    id("com.gradle.enterprise").version("3.4.1")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}

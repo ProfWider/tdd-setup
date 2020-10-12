43834de3c809ae495126a8b555ef63ab5e4ac16a
root <root@DESKTOP-CB4TTHL.localdomain>
2020-10-12 21:54:16 +0200

    added test for bnasic calculations

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -6,14 +6,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Retro calculator")
 class CalculatorTest {
+    private Calculator calc = new Calculator();
+
     @Test
     @DisplayName("can do 2 + 2")
     void calculatorCanDoTwoPlusTwo() {
-        Calculator calc = new Calculator();
         calc.pressDigitKey(2);
         calc.pressOperationKey("+");
         calc.pressDigitKey(2);
         calc.pressEquals();
         assertEquals("4", calc.readScreen());
+    }
+
+    @Test
+    @DisplayName("can do basic subtraction")
+    void calculatorBasicMinus() {
+        calc.pressDigitKey(2);
+        calc.pressOperationKey("-");
+        calc.pressDigitKey(2);
+        calc.pressEquals();
+        assertEquals("0", calc.readScreen());
+    }
+
+    @Test
+    @DisplayName("can do basic multiplication")
+    void calculatorBasicMultiplication() {
+        calc.pressDigitKey(2);
+        calc.pressOperationKey("x");
+        calc.pressDigitKey(2);
+        calc.pressEquals();
+        assertEquals("4", calc.readScreen());
+    }
+
+    @Test
+    @DisplayName("can do basic division")
+    void calculatorBasicDivision() {
+        calc.pressDigitKey(2);
+        calc.pressOperationKey("/");
+        calc.pressDigitKey(2);
+        calc.pressEquals();
+        assertEquals("1", calc.readScreen());
     }
 }

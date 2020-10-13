1bec476893f4eb3e9819428c42e6961a57cc2acd
root <root@DESKTOP-CB4TTHL.localdomain>
2020-10-13 10:21:31 +0200

    added two more tests to check pressDotKey and pressNegative

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -76,4 +76,26 @@ class CalculatorTest {
         assertEquals("8", calc.readScreen());
         calc.pressDigitKey(2);
     }
+
+    @Test
+    @DisplayName("only one dot press allowed")
+    void calculatorPressDotAgain() {
+        calc.pressDigitKey(2);
+        calc.pressDotKey();
+        calc.pressDigitKey(5);
+        assertEquals("2.5", calc.readScreen());
+        calc.pressDotKey();
+        assertEquals("2.5", calc.readScreen());
+    }
+
+    @Test
+    @DisplayName("pressing nagative again makes number positive")
+    void calculatorPressNegativeAgain() {
+        calc.pressDigitKey(2);
+        calc.pressNegative();
+        assertEquals("-2", calc.readScreen());
+        calc.pressNegative();
+        assertEquals("2", calc.readScreen());
+    }
+
 }

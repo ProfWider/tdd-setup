b2c6986573e39e30606a7f6e55119f14635f5136
s0569459 <s0569459@student.htw-berlin.de>
2020-10-20 14:42:06 +0200

    first red test

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -28,27 +28,27 @@ class CalculatorTest {
         assertEquals("8", calculator.readScreen());
     }
 
-//    @Test
-//    @DisplayName("should display 0 after pressing clear button")
-//    void calculatorClearsScreen() {
-//        Calculator cal = new Calculator();
-//        cal.pressDigitKey(5);
-//        cal.pressClearKey();
-//        assertEquals("0", cal.readScreen());
-//    }
+    @Test
+    @DisplayName("should display 0 after pressing clear button")
+    void calculatorClearsScreen() {
+        Calculator cal = new Calculator();
+        cal.pressDigitKey(5);
+        cal.pressClearKey();
+        assertEquals("0", cal.readScreen());
+    }
 //
-//    @Test
-//    @DisplayName("Should display the correctly placed dot after pressing the dot button")
-//    void calSetsDotCorrectly() {
-//        Calculator c = new Calculator();
-//        c.pressDigitKey(9);
-//        c.pressDigitKey(0);
-//        c.pressDotKey();
-//        c.pressDigitKey(1);
-//        c.pressDigitKey(5);
-//        c.readScreen();
-//        assertEquals("90.15", c.readScreen());
-//    }
+    @Test
+    @DisplayName("Should display the correctly placed dot after pressing the dot button")
+    void calSetsDotCorrectly() {
+        Calculator c = new Calculator();
+        c.pressDigitKey(9);
+        c.pressDigitKey(0);
+        c.pressDotKey();
+        c.pressDigitKey(1);
+        c.pressDigitKey(5);
+        c.readScreen();
+        assertEquals("90.15", c.readScreen());
+    }
 //
 //    @Test
 //    @DisplayName("Should display negative value of input")

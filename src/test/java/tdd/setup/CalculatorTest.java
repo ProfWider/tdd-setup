a4bb2848d789b804af6d638d23ada35a256186d1
Tuce Yemisen <tuce_yemisen@hotmail.de>
2020-10-22 17:40:53 +0200

    test commit 1

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("Retro calculator")
 class CalculatorTest {
     @Test
-    @DisplayName("should display result after adding two positive numbers")
+    @DisplayName("should display result after adding positive numbers")
     void calculatorCanDoTwoPlusTwo() {
         Calculator calc = new Calculator();
         calc.pressDigitKey(2);
@@ -15,5 +15,19 @@ class CalculatorTest {
         calc.pressDigitKey(2);
         calc.pressEquals();
         assertEquals("4", calc.readScreen());
+    }
+
+    //Function which works.
+    @Test
+    @DisplayName("should display result after dividing a positive and negative numbers")
+    void calculatorCanDivideMinusTwentyByFour() {
+        Calculator calc = new Calculator();
+        calc.pressNegative();
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(0);
+        calc.pressOperationKey("/");
+        calc.pressDigitKey(4);
+        calc.pressEquals();
+        assertEquals("-5", calc.readScreen());
     }
 }

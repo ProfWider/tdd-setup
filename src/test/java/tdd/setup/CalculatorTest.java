b19dfbe531abb569b27e88f3e644175a87ddef1d
MarieJager <marie.jager@student.htw-berlin.de>
2020-10-22 13:32:58 +0200

    added test calculatorCanAddDecimalDigits, test failed

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -16,14 +16,45 @@ class CalculatorTest {
         calc.pressEquals();
         assertEquals("4", calc.readScreen());
     }
-    
+
     @Test
     @DisplayName("should display result after subtracting the second number of the first one")
-   void calculatorCanDoThreeMinusTwo() {
-       Calculator calc = new Calculator();
-       calc.pressDigitKey(3);
-       calc.pressOperationKey("-");
-       calc.pressDigitKey(2);
-       calc.pressEquals();
-       assertEquals("1", calc.readScreen());
+    void calculatorCanDoThreeMinusTwo() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(3);
+        calc.pressOperationKey("-");
+        calc.pressDigitKey(2);
+        calc.pressEquals();
+        assertEquals("1", calc.readScreen());
+
+    }
+
+    @Test
+    @DisplayName("should display result after adding two decimal digits")
+    void calculatorCanAddDecimalDigits() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(1);
+        calc.pressDotKey();
+        calc.pressDigitKey(2);
+        calc.pressOperationKey("+");
+        calc.pressDigitKey(3);
+        calc.pressDotKey();
+        calc.pressDigitKey(6);
+        calc.pressEquals();
+        assertEquals("4.8", calc.readScreen());
+    }
+
+    @Test
+    @DisplayName("should display result after subtracting two negative numbers")
+    void calculatorCanSubtractNegatives() {
+
+    }
+
+    @Test
+    @DisplayName("should display result after multiplying a negative with a positive number")
+    void calculatorCanMultiplyPositiveAndNegative() {
+
+    }
 }
+
+

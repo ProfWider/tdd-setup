ebd57d618cd2d8e4a78826db080576c7b4c171ac
Al-Harethi <Ahmed.al-harethi@student.htw-berlin.de>
2020-10-22 11:59:00 +0200

    first red test

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -6,16 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Retro calculator")
 class CalculatorTest {
-    @Test
-    @DisplayName("should display result after adding two positive numbers")
-    void calculatorCanDoTwoPlusTwo() {
-        Calculator calc = new Calculator();
-        calc.pressDigitKey(2);
-        calc.pressOperationKey("+");
-        calc.pressDigitKey(2);
-        calc.pressEquals();
-        assertEquals("4", calc.readScreen());
-    }
+//    @Test
+//   @DisplayName("should display result after adding two positive numbers")
+//    void calculatorCanDoTwoPlusTwo() {
+//        Calculator calc = new Calculator();
+//        calc.pressDigitKey(2);
+//        calc.pressOperationKey("+");
+//        calc.pressDigitKey(2);
+//        calc.pressEquals();
+//        assertEquals("4", calc.readScreen());
+//    }
 
     @Test
     @DisplayName("should display result after multiplying two numbers, one of them is negative")

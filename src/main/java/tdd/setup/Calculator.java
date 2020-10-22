183b18251f67c063b0f4e226e153a0926bad6c99
Tuce Yemisen <tuce_yemisen@hotmail.de>
2020-10-22 17:49:54 +0200

    test commit 2 green

--- src/main/java/tdd/setup/Calculator.java
+++ src/main/java/tdd/setup/Calculator.java
@@ -3,7 +3,8 @@ package tdd.setup;
 // behaviour inspired by https://www.online-calculator.com/
 public class Calculator {
 
-    private String screen = "0";
+    //0 in screen causes faulty display of digits
+    private String screen = "";
 
     private double latestValue;
 

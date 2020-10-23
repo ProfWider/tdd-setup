0026acc0c655c6cc8190c0ae1112f90f02b5ab3a
John-SimonB <72495440+John-SimonB@users.noreply.github.com>
2020-10-23 14:32:53 +0200

    grüner Test für die Methode calculatorCanDoTwoMinusTwo roter Test für die Methode calculatorCanDoDotAndAddANegativeNumber

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -28,4 +28,19 @@ class CalculatorTest {
         System.out.println(calc.readScreen());
 
     }
+    @Test
+    @DisplayName("test for adding a decimal number with a natural number")
+    void calculatorCanDoDotAndAddANegativeNumber() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(3);
+        calc.pressDotKey();
+        calc.pressDigitKey(3);
+        calc.pressOperationKey("+");
+        calc.pressNegative();
+        calc.pressDigitKey(2);
+        calc.pressEquals();
+        System.out.println(calc.readScreen());
+        assertEquals("1.3", calc.readScreen());
+    }
+
 }

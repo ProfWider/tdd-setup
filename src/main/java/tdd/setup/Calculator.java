87329ac8d56aba87aa6f26062dbd236d6c89e416
Diart <s0570985@htw-berlin.de>
2020-10-23 16:21:18 +0200

    3. Test (Bugfix)

--- src/main/java/tdd/setup/Calculator.java
+++ src/main/java/tdd/setup/Calculator.java
@@ -12,19 +12,20 @@ public class Calculator {
     public String readScreen() {
         return screen;
     }
-    public void pressDigitKey(int digit) {
-        if(digit > 9 || digit < 0) throw new IllegalArgumentException();
 
-        if(latestOperation.isEmpty()) {
+    public void pressDigitKey(int digit) {
+        if (digit > 9 || digit < 0) throw new IllegalArgumentException();
+
+        if (latestOperation.isEmpty()) {
             screen = screen + digit;
             helper = false;
         } else {
-            if (helper==false) {
+            if (helper == false) {
                 latestValue = Double.parseDouble(screen);
                 screen = "0";
-                helper=true;
+                helper = true;
                 screen = screen + digit;
-            }else{
+            } else {
                 screen = screen + digit;
             }
 
@@ -33,35 +34,38 @@ public class Calculator {
         }
     }
 
-    public void pressClearKey(){
-        screen = "0";
-        latestOperation = "";
-        latestValue = 0.0;
+    public void pressClearKey() {
+        if (!latestOperation.isEmpty() && screen!="0") {
+            screen = "0";
+        } else {
+            screen = "0";
+            latestOperation = "";
+            latestValue = 0.0;
+
+        }
+    }
+        public void pressOperationKey (String operation){
+            latestOperation = operation;
+        }
+
+        public void pressDotKey () {
+            if (!screen.endsWith(".")) screen = screen + ".";
+        }
+
+        public void pressNegative () {
+            screen = screen.startsWith("-") ? screen.substring(1) : "-" + screen;
+        }
+
+        public void pressEquals () {
+            var result = switch (latestOperation) {
+                case "+" -> latestValue + Double.parseDouble(screen);
+                case "-" -> latestValue - Double.parseDouble(screen);
+                case "x" -> latestValue * Double.parseDouble(screen);
+                case "/" -> latestValue / Double.parseDouble(screen);
+                default -> throw new IllegalArgumentException();
+            };
+            screen = Double.toString(result);
+            if (screen.endsWith(".0")) screen = screen.substring(0, screen.length() - 2);
+        }
 
     }
-
-    public void pressOperationKey(String operation)  {
-        latestOperation = operation;
-    }
-
-    public void pressDotKey() {
-        if(!screen.endsWith(".")) screen = screen + ".";
-    }
-
-    public void pressNegative() {
-        screen = screen.startsWith("-") ? screen.substring(1) : "-" + screen;
-    }
-
-    public void pressEquals() {
-        var result = switch(latestOperation) {
-            case "+" -> latestValue + Double.parseDouble(screen);
-            case "-" -> latestValue - Double.parseDouble(screen);
-            case "x" -> latestValue * Double.parseDouble(screen);
-            case "/" -> latestValue / Double.parseDouble(screen);
-            default -> throw new IllegalArgumentException();
-        };
-        screen = Double.toString(result);
-        if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
-    }
-
-}

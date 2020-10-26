b580c9632c52e86021f8adac4320f40f4fa7dd7c
Henry Le <s0570716@htw-berlin.de>
2020-10-26 20:01:17 +0100

    1. roter Test

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -7,17 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("Retro calculator")
 class CalculatorTest {
     @Test
-    @DisplayName("should display result after multiply one positive and one negative number")
-    void calculatorCanDoMultiplyTwo()
-    {
-        Calculator calc = new Calculator();
-
-        calc.pressDigitKey(2);
-        calc.pressNegative();
-        calc.pressDigitKey(5);
-        calc.pressOperationKey("x");
-        calc.pressDigitKey(2);
-        calc.pressEquals();
-        assertEquals("-50", calc.readScreen());
+    @DisplayName("should display result after adding one positive number to positive number with dot")
+    void calculatorCanDoAddingNumbersWithDot() {
+        Calculator calc2 = new Calculator();
+        calc2.pressDigitKey(2);
+        calc2.pressOperationKey("+");
+        calc2.pressDigitKey(1);
+        calc2.pressDotKey();
+        calc2.pressDigitKey(2);
+        calc2.pressEquals();
+        assertEquals("3.2", calc2.readScreen());
     }
 }

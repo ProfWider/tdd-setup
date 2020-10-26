2cac19b985cfd6a49afad3f19cf72d90970f791a
root <root@DESKTOP-DNLFJVU.localdomain>
2020-10-26 12:15:45 +0100

    green test

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -17,39 +17,19 @@ class CalculatorTest {
         assertEquals("4", calc.readScreen());
     }
 
-    //first extension of the existing test
+    //green test
     @Test
-    @DisplayName("should display result after subtract two positive numbers")
-    void calculatorCanDoFiveMinusThree() {
-        Calculator calc = new Calculator();
-        calc.pressDigitKey(5);
-        calc.pressOperationKey("-");
-        calc.pressDigitKey(3);
-        calc.pressEquals();
-        assertEquals("2", calc.readScreen());
-    }
-    //second extension of the existing test
-    @Test
-    @DisplayName("should display result after multiplying two positive numbers")
+    @DisplayName("should display result after multiplying a positive with a negative number")
     void calculatorCanDoFiveMultiplyingThree() {
         Calculator calc = new Calculator();
         calc.pressDigitKey(5);
         calc.pressOperationKey("x");
+        calc.pressNegative();
         calc.pressDigitKey(3);
         calc.pressEquals();
-        assertEquals("15", calc.readScreen());
+        assertEquals("-15", calc.readScreen());
     }
 
-    //red test
-    @Test
-    @DisplayName("8 x -2 = -16")
-    void calculatorMultiplyingPositiveAndNegative() {
-        Calculator calc = new Calculator();
-        calc.pressDigitKey(8);
-        calc.pressOperationKey("x");
-        calc.pressNegative();
-        calc.pressDigitKey(2);
-        calc.pressEquals();
-        assertEquals("16", calc.readScreen());
-    }
+
 }
+

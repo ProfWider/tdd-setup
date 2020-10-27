d5ab64ec340a13dd3ef4c6517b44ea7be4716c2c
Magda <s0574860@htw-berlin.de>
2020-10-27 17:09:31 +0100

    first red Test fixed

--- src/main/java/tdd/setup/Calculator.java
+++ src/main/java/tdd/setup/Calculator.java
@@ -3,7 +3,7 @@ package tdd.setup;
 // behaviour inspired by https://www.online-calculator.com/
 public class Calculator {
 
-    private String screen = "0";
+    private String screen = "";
 
     private double latestValue;
 
@@ -38,7 +38,13 @@ public class Calculator {
     }
 
     public void pressNegative() {
-        screen = screen.startsWith("-") ? screen.substring(1) : "-" + screen;
+
+        if (screen.startsWith("0")) {
+            screen = "-" + readScreen();
+        }
+        else {
+            screen = screen.startsWith("-") ? screen.substring(1) : "-" + screen;
+        }
     }
 
     public void pressEquals() {

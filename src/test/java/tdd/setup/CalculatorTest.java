4b717f530127ed3e943b4786c0f1ade2d07e387d
Al-Harethi <Ahmed.al-harethi@student.htw-berlin.de>
2020-10-27 15:56:12 +0100

    first green test

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -16,4 +16,48 @@ class CalculatorTest {
         calc.pressEquals();
         assertEquals("4", calc.readScreen());
     }
+
+    @Test
+    @DisplayName("should display 0 after pressing clear Key")
+    void calculatorCanClearScreen() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(2);
+        calc.pressOperationKey("x");
+        calc.pressNegative();
+        calc.pressDigitKey(2);
+        calc.pressEquals();
+        calc.pressClearKey();
+        assertEquals("0",calc.readScreen());
+    }
+
+    @Test
+    @DisplayName("should display result after multiplying two numbers, one of them is negative")
+    void calculatorCanDoTwoMultiplyMinusTwo() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(2);
+        calc.pressOperationKey("x");
+        calc.pressNegative();
+        calc.pressDigitKey(2);
+        calc.pressEquals();
+        assertEquals("-4", calc.readScreen());
+    }
+
+
+
+    @Test
+    @DisplayName("should display the correct number after minusing two numbers, " +
+            "one of them with dot(double or float number)")
+    void calculatorCanUseDotKey() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(5);
+        calc.pressDotKey();
+        calc.pressDigitKey(5);
+        calc.pressOperationKey("-");
+        calc.pressDigitKey(1);
+        calc.pressEquals();
+        assertEquals("4.5",calc.readScreen());
+    }
+
+
+
 }

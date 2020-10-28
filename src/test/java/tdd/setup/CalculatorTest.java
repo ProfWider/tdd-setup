efbf18dc6e3d98be9554ae4f7921be616685e363
Henry Le <s0570716@htw-berlin.de>
2020-10-28 13:04:05 +0100

    2. roter Test

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -42,4 +42,16 @@ class CalculatorTest {
         c.pressEquals();
         assertEquals("2", c.readScreen());
     }
+    @Test
+    @DisplayName("should display result after adding one positive number to positive number with dot")
+    void calculatorCanDoAddingNumbersWithDot() {
+        Calculator calc2 = new Calculator();
+        calc2.pressDigitKey(2);
+        calc2.pressOperationKey("+");
+        calc2.pressDigitKey(1);
+        calc2.pressDotKey();
+        calc2.pressDigitKey(2);
+        calc2.pressEquals();
+        assertEquals("3.2", calc2.readScreen());
+    }
 }

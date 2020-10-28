f8d8a2478c05733116583a0c0d2b1fe4c33bacd0
root <root@DESKTOP-DNLFJVU.localdomain>
2020-10-28 11:58:25 +0100

    red test calculatorCanDoDecimal

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -41,4 +41,16 @@ class CalculatorTest {
         calc.pressEquals();
         assertEquals("256", calc.readScreen());
     }
+
+    //test case three
+    @Test
+    @DisplayName("should return a decimal number on display")
+    void calculatorCanDoDecimal() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(5);
+        calc.pressDotKey();
+        calc.pressDigitKey(3);
+        // calc.pressEquals();
+        assertEquals("5.3", calc.readScreen());
+    }
 }

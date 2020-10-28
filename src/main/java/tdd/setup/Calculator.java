50e311feaa4eea9cbc9d88da45dd72d23278f9ae
fluke00700 <teerapat.thiti@gmail.com>
2020-10-28 10:13:35 +0100

    Fix zum letzten Test

--- src/main/java/tdd/setup/Calculator.java
+++ src/main/java/tdd/setup/Calculator.java
@@ -2,63 +2,72 @@ package tdd.setup;
 
 // behaviour inspired by https://www.online-calculator.com/
 public class Calculator {
-private String screen = "0";
 
-private double latestValue;
+        private String screen = "0";
 
-private boolean equalPressed = false;
+        private double latestValue;
 
-private String latestOperation = "";
+        private boolean equalPressed = false;
 
-public String readScreen() {
-        if(screen.startsWith("0"))
-        {
-        screen = screen.substring(1);
+        private String latestOperation = "";
+
+        public String readScreen() {
+                if(screen.startsWith("0"))
+                {
+                        screen = screen.substring(1);
+                }
+                return screen;
         }
-        return screen;
-        }
-public void pressDigitKey(int digit) {
-        if(digit > 9 || digit < 0) throw new IllegalArgumentException();
+        public void pressDigitKey(int digit) {
+                if(digit > 9 || digit < 0) throw new IllegalArgumentException();
 
-        if(latestOperation.isEmpty()) {
-        screen = screen + String.valueOf(digit);
-        latestValue = Double.parseDouble(screen);
-        } else {
+                if(latestOperation.isEmpty()) {
+                        screen = screen + String.valueOf(digit);
+                        latestValue = Double.parseDouble(screen);
+                } else {
 
-        screen = screen + String.valueOf(digit);
+                        screen = screen + String.valueOf(digit);
 
-        }
-        }
-
-public void pressClearKey() {
-        latestOperation = "";
-        latestValue = 0.0;
+                }
         }
 
-public void pressOperationKey(String operation)  {
-        latestOperation = operation;
-        screen = "0";
-        ;
+        public void pressClearKey() {
+                latestOperation = "";
+                latestValue = 0.0;
         }
 
-public void pressDotKey() {
-        if(!screen.endsWith(".")) screen = screen + ".";
+        public void pressOperationKey(String operation)  {
+                latestOperation = operation;
+                screen = "0";
+                ;
         }
 
-public void pressNegative() {
-        screen = screen.startsWith("-") ? screen.substring(1) : "-" + screen;
+        public void pressDotKey() {
+                if(!screen.endsWith(".")) screen = screen + ".";
         }
 
-public void pressEquals() {
+        public void pressNegative() {
+                if(screen.startsWith("0"))
+                {
+                        screen = screen.substring(1);
+                        screen = "-" + screen;
+                }
+                else {
+                        screen = "-" + screen;
+                }
+        }
 
-        var result = switch(latestOperation) {
-        case "+" -> latestValue + Double.parseDouble(screen);
-        case "-" -> latestValue - Double.parseDouble(screen);
-        case "x" -> latestValue * Double.parseDouble(screen);
-        case "/" -> latestValue / Double.parseDouble(screen);
-default -> throw new IllegalArgumentException();
-        };
-        screen = Double.toString(result);
-        if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
+
+        public void pressEquals() {
+
+                var result = switch(latestOperation) {
+                        case "+" -> latestValue + Double.parseDouble(screen);
+                        case "-" -> latestValue - Double.parseDouble(screen);
+                        case "x" -> latestValue * Double.parseDouble(screen);
+                        case "/" -> latestValue / Double.parseDouble(screen);
+                        default -> throw new IllegalArgumentException();
+                };
+                screen = Double.toString(result);
+                if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
         }
-        }
+}

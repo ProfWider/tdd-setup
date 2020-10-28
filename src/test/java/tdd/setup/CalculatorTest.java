806b92a5ca95a886cb0186b4723c48043ebaca8a
Henry Le <s0570716@htw-berlin.de>
2020-10-28 13:02:07 +0100

    1. roter Test

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -30,4 +30,16 @@ class CalculatorTest {
         assertEquals("-50", calc1.readScreen());
     }
 
+    @Test
+    @DisplayName("should display the number after using clear key")
+    void calculatorCanShowNumberAfterUsingClearKey() {
+        Calculator c = new Calculator();
+        c.pressDigitKey(1);
+        c.pressOperationKey("x");
+        c.pressDigitKey(1);
+        c.pressClearKey();
+        c.pressDigitKey(2);
+        c.pressEquals();
+        assertEquals("2", c.readScreen());
+    }
 }

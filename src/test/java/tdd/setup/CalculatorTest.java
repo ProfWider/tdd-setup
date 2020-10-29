e37112ddb4e140b621d41fb2208eb6f698aafa83
realphilip <pajach@pm.me>
2020-10-29 07:29:11 +0100

    add green test for clear key

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -6,14 +6,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Retro calculator")
 class CalculatorTest {
+
+    private Calculator calc = new Calculator();
     @Test
     @DisplayName("should display result after adding two positive numbers")
     void calculatorCanDoTwoPlusTwo() {
-        Calculator calc = new Calculator();
         calc.pressDigitKey(2);
         calc.pressOperationKey("+");
         calc.pressDigitKey(2);
         calc.pressEquals();
         assertEquals("4", calc.readScreen());
+    }
+
+    @Test
+    @DisplayName("should revert screen to 0")
+    void calculatorCanClearScreen(){
+        calc.pressDigitKey(2);
+        calc.pressOperationKey("+");
+        calc.pressDigitKey(2);
+        calc.pressEquals();
+        calc.pressClearKey();
+        assertEquals("0", calc.readScreen());
     }
 }

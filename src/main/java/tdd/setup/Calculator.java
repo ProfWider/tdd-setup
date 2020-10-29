b0b00bc445e7a0ce79c6c919ecf8836259f2d459
realphilip <pajach@pm.me>
2020-10-29 07:34:36 +0100

    fix for percentage operator, test now green

--- src/main/java/tdd/setup/Calculator.java
+++ src/main/java/tdd/setup/Calculator.java
@@ -43,6 +43,7 @@ public class Calculator {
 
     public void pressEquals() {
         var result = switch(latestOperation) {
+            case "%" -> latestValue / 100;
             case "+" -> latestValue + Double.parseDouble(screen);
             case "-" -> latestValue - Double.parseDouble(screen);
             case "x" -> latestValue * Double.parseDouble(screen);

fc6466985f3f1e0b95264bcfb36fa4b8e66d61f4
kemalghifari <mkemalgr@gmail.com>
2020-10-29 15:49:10 +0100

    fixed bug in the first red test

--- src/main/java/tdd/setup/Calculator.java
+++ src/main/java/tdd/setup/Calculator.java
@@ -19,7 +19,11 @@ public class Calculator {
             screen = screen + digit;
         } else {
             latestValue = Double.parseDouble(screen);
-            screen = Integer.toString(digit);
+            if(!screen.contains(".")){
+                screen = Integer.toString(digit);
+            } else {
+                screen = "0" + "." + digit;
+            }
         }
     }
 
@@ -34,7 +38,7 @@ public class Calculator {
     }
 
     public void pressDotKey() {
-        if(!screen.endsWith(".")) screen = screen + ".";
+        if(!screen.contains(".")) screen = screen + ".";
     }
 
     public void pressNegative() {

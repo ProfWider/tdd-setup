13479dac62764959c05e2e955bcb4618d7245b31
root <root@Yannick-Laptop.localdomain>
2020-10-29 11:48:55 +0100

    Bugfix zweiter roter Test

--- src/main/java/tdd/setup/Calculator.java
+++ src/main/java/tdd/setup/Calculator.java
@@ -12,11 +12,17 @@ public class Calculator {
     public String readScreen() {
         return screen;
     }
+
     public void pressDigitKey(int digit) {
         if(digit > 9 || digit < 0) throw new IllegalArgumentException();
 
         if(latestOperation.isEmpty()) {
-            screen = screen + digit;
+            if (screen == "0") {
+                screen = Integer.toString(digit);
+            }
+            else {
+                screen = screen + digit;
+            }
         } else {
             latestValue = Double.parseDouble(screen);
             screen = Integer.toString(digit);

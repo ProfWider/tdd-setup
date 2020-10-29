5a49012d506438c7fcbba346730c78e67a480334
vasilios-k <s0569216@htw-berlin.de>
2020-10-29 16:37:54 +0100

    s2. red test

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -38,4 +38,21 @@ class CalculatorTest {
         calc.pressDotKey();
         assertEquals("5.2", calc.readScreen());
     }
+
+    @Test
+    @DisplayName("should calculate with multiple numbers")
+    void calculatorCanDoMultipleCalculations(){
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(2);
+        calc.pressDotKey();
+        calc.pressDigitKey(4);
+        calc.pressOperationKey("+");
+        calc.pressDigitKey(5);
+        calc.pressOperationKey("-");
+        calc.pressDigitKey(3);
+        calc.pressEquals();
+        assertEquals("4.4", calc.readScreen());
+
+
+    }
 }

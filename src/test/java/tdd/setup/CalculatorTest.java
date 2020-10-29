332fe197b2722fc1f97fa50d94012377d1649d55
EmadBaroudi <emadaghyadbaroudi@gmail.com>
2020-10-29 14:16:59 +0100

    First Green Test

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -16,4 +16,16 @@ class CalculatorTest {
         calc.pressEquals();
         assertEquals("4", calc.readScreen());
     }
+    // First Green Test
+    @Test
+    @DisplayName("should Clear the screen and display 0")
+    void calculatorCanClearScreen() {
+        Calculator calc1 = new Calculator();
+        calc1.pressDigitKey(2);
+        calc1.pressOperationKey("+");
+        calc1.pressDigitKey(2);
+        calc1.pressEquals();
+        calc1.pressClearKey();
+        assertEquals("0",calc1.readScreen());
+    }
 }

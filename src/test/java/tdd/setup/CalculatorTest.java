01f78a12a29e40b4488679e7ba890025ee75459d
root <root@Yannick-Laptop.localdomain>
2020-10-29 11:36:12 +0100

    Erster, grüner Test

--- src/test/java/tdd/setup/CalculatorTest.java
+++ src/test/java/tdd/setup/CalculatorTest.java
@@ -16,4 +16,15 @@ class CalculatorTest {
         calc.pressEquals();
         assertEquals("4", calc.readScreen());
     }
+
+    @Test
+    @DisplayName("should display result after subtracting two positive numbers")
+    void calculatorCanSubtract() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(5);
+        calc.pressOperationKey("-");
+        calc.pressDigitKey(6);
+        calc.pressEquals();
+        assertEquals("-1", calc.readScreen());
+    }
 }

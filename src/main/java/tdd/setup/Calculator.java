86ee39f1f08c0e8e091f3f6a80d476d8d10a1706
Leon Hauch <hauchleon@gmail.com>
2020-10-30 16:06:16 +0100

    green test for third bugfix

--- src/main/java/tdd/setup/Calculator.java
+++ src/main/java/tdd/setup/Calculator.java
@@ -61,5 +61,8 @@ public class Calculator {
         };
         screen = Double.toString(result);
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
+        if (screen=="Infinity"){
+            screen="Error";
+        }
     }
 }

eafc977be6dd9ea53395d53c85343d0ec6ab8df8
Ahmad <ahmad.matar@student.htw-berlin.de>
2020-10-30 12:20:17 +0100

    bug fix press 2x dot

--- src/main/java/tdd/setup/Calculator.java
+++ src/main/java/tdd/setup/Calculator.java
@@ -40,7 +40,7 @@ public class Calculator {
     }
 
     public void pressDotKey() {
-        if(!screen.endsWith(".")) screen = screen + ".";
+        if(!screen.contains(".")) screen = screen + ".";
     }
 
     public void pressNegative() {
